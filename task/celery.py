493a18da16c679310e3a7a061ac05dc0933dca5e
EarvinInk <87079067+EarvinInk@users.noreply.github.com>
2022-10-14 10:55:56 +0530

    2

--- task/celery.py
+++ task/celery.py
@@ -2,8 +2,8 @@ import os
 from celery import Celery
 
 # setting the Django settings module.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celerytask.settings')
-app = Celery('celerytask')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task.settings')
+app = Celery('task')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 # app.conf.broker_transport_options = {'visibility_timeout': 3600}  # 1 hour.
 # Looks up for task modules in Django applications and loads them
